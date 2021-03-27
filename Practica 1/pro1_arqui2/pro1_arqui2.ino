c1239ef22e6318c94235292fbb0cca02333ab7aa
Katherine <sanchezkathy29@gmail.com>
2021-03-26 18:52:41 -0600

    Membrana

--- Practica 1/pro1_arqui2/pro1_arqui2.ino
+++ Practica 1/pro1_arqui2/pro1_arqui2.ino
@@ -1,78 +1,79 @@
-//VAR ACELEROMETRO
-const int x_out = A1; /* connect x_out of module to A1 of UNO board */
-const int y_out = A2; /* connect y_out of module to A2 of UNO board */
-const int z_out = A3; /* connect z_out of module to A3 of UNO board */
-
 //VAR BUZZER Y TIEMPO
-const int buzzer=2;
-unsigned long segundos = 0;
+unsigned long segundos = 60;
 unsigned long segundosArduino = 64;
 unsigned long minutos = 0;
-
-
-
+int motor = 13;
+int buzz = 9;
+int botonMembrana = 12;
+bool presionado = false;
+bool switcheo = false;
+int repeticion = 0;
+int ritmo = 5;
+bool prueba = true;
 //----------FIN VARIABLES-----------
 
 void setup() {
-  pinMode(buzzer, OUTPUT);  //definir pin como salida
-  pinMode(x_out, INPUT);  
-  pinMode(y_out, INPUT);  
-  pinMode(z_out, INPUT);  
-   
+  pinMode(motor, OUTPUT);
+  digitalWrite(motor, HIGH);
   Serial.begin(9600);
+  
+  pinMode(buzz, OUTPUT);
+  pinMode(botonMembrana, OUTPUT);
+ 
 }
 
 void loop(){
+
+  minuto();
+  activarMembranaBoton();
+  
+  limiteRitmo();
+ 
+}
+
+void limiteRitmo(){
+  if(prueba){ //suponiendo que el ritmoMaximo es =10
+    digitalWrite(motor, LOW);
+    delay(10000);
+    digitalWrite(motor, HIGH);
+    prueba = false;
+  } 
   
 }
 
-bool minuto(){
-  unsigned long tiempo =  (millis())/1000;
-  if(tiempo == segundosArduino){
-    segundos += 64;
+void activarMembranaBoton(){
+  if(digitalRead(botonMembrana)== HIGH) {
+      presionado = true;
+    }else {
+      if(presionado){
+        if(switcheo){
+          digitalWrite(motor, LOW);
+        }else{
+          digitalWrite(motor, HIGH);
+        }
+        switcheo = !switcheo;
+        presionado = false;
+      }
+    }
+}
+
+void minuto(){
+  unsigned long segundosArd =  (millis())/1000;
+  if(segundosArd == segundos){
+    segundos += 60;
     minutos++;
+    repeticion++;
     //Encender buzzer
-    digitalWrite(buzzer,HIGH);//Encender el buzzer
-    delay(1000);//Esperar el tiempo programado
-  }  
+    analogWrite(buzz, 50);
+    delay(1000);//Buzzer sonando
+    //Apagar buzzer
+    analogWrite(buzz, 0);
+    Serial.print("Min: ");
+    Serial.println(minutos);
+  }
 }
 
-void velocidad(){
-  int x_adc_value, y_adc_value, z_adc_value;
-  double x_g_value, y_g_value, z_g_value;
-  double roll, pitch, yaw;
-  x_adc_value = analogRead(x_out); /* Digital value of voltage on x_out pin */
-  y_adc_value = analogRead(y_out); /* Digital value of voltage on y_out pin */
-  z_adc_value = analogRead(z_out); /* Digital value of voltage on z_out pin */
-  Serial.print("x = ");
-  Serial.print(x_adc_value);
-  Serial.print("\t\t");
-  Serial.print("y = ");
-  Serial.print(y_adc_value);
-  Serial.print("\t\t");
-  Serial.print("z = ");
-  Serial.print(z_adc_value);
-  Serial.print("\t\t");
-  //delay(100);
+
+void rendicion(){
   
-  x_g_value = ( ( ( (double)(x_adc_value * 5)/1024) - 1.65 ) / 0.330 ); /* Acceleration in x-direction in g units */
-  y_g_value = ( ( ( (double)(y_adc_value * 5)/1024) - 1.65 ) / 0.330 ); /* Acceleration in y-direction in g units */
-  z_g_value = ( ( ( (double)(z_adc_value * 5)/1024) - 1.80 ) / 0.330 ); /* Acceleration in z-direction in g units */  
-
-  //formulas:
-  // vf = v0+at
-  // d = vt
-
-   //ESTO NO SÉ QUE ONDA
-  roll = ( ( (atan2(y_g_value,z_g_value) * 180) / 3.14 ) + 180 ); /* Formula for roll */
-  pitch = ( ( (atan2(z_g_value,x_g_value) * 180) / 3.14 ) + 180 ); /* Formula for pitch */
-  //yaw = ( ( (atan2(x_g_value,y_g_value) * 180) / 3.14 ) + 180 ); /* Formula for yaw */
-  /* Not possible to measure yaw using accelerometer. Gyroscope must be used if yaw is also required */
-  Serial.print("Roll = ");
-  Serial.print(roll);
-  Serial.print("\t");
-  Serial.print("Pitch = ");
-  Serial.print(pitch);
-  Serial.print("\n\n");
-  delay(1000);
 }
