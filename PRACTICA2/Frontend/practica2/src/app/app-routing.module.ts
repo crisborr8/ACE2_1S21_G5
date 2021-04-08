357c9215e2f5bd4c9ba8efe790bbf9586008cf84
Alejandro Marín <3019371840101@Ingenieria.usac.edu.gt>
2021-04-07 21:38:57 -0600

    201602855-PaginaInicio

--- PRACTICA2/Frontend/practica2/src/app/app-routing.module.ts
+++ PRACTICA2/Frontend/practica2/src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {InicioComponent} from './components/inicio/inicio.component'
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'', redirectTo:'Inicio',pathMatch:'full'},
+  {path:'Inicio',component:InicioComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
