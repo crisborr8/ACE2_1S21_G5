ddf6f6e90674a9efe5ee08c6e9a6b8c29eb3c176
Nery Eduardo Herrera <tesco.herrera.edu@gmail.com>
2021-05-13 21:55:46 -0600

    [ADD] fix hour

--- ProyectoFinal/Backend/Server/src/routes/index.js
+++ ProyectoFinal/Backend/Server/src/routes/index.js
@@ -717,7 +717,7 @@ function incrementarTime(tiempoInicial)
     var segundo = tiemposplitted[2];
 
     var segundoint = Number(segundo);
-    segundoint = segundoint + 5;
+    segundoint = segundoint + 1;
     segundo = String(segundoint)
 
     var agregadohora = 0;
